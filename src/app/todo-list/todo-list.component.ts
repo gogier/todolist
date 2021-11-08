c026474aad670ad198bb8f0c7df2cc269a6dd9dc
Guillaume OGIER <guillaume.ogier.ext@louisvuitton.com>
2021-11-08 16:46:47 +0100

    #4 - Create a task : add the category and actor parsing : first simple release

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,6 +4,8 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import { FormBuilder } from '@angular/forms';
 import { currentTasks, Task } from '../tasks';
 
+import { v4 as uuidv4 } from 'uuid';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -16,28 +18,56 @@ export class TodoListComponent {
 
   tasks = currentTasks;
 
+  /* On init : SORT Task */
   ngOnInit() {
     this.tasks.sort((a, b) => a.order > b.order && 1 || -1);
   }  
 
   constructor(private formBuilder: FormBuilder) {}
 
-  share() {
-    window.alert('The task has been shared!');
-  }
 
+
+  /* ADD Task Action */
   addToTodoList(): void {
     // Process checkout data here
-    this.tasks.push(this.newTaskForm.value);
-    console.warn('Your item has been submitted', this.newTaskForm.value);
+
+    console.log('Check RegEx from title value');
+    const regexpTask = /(\[.*\])(.*)(\@\w*)/;
+    var match = this.newTaskForm.value.title.match(regexpTask);
+
+    var newTaskTitle = this.newTaskForm.value.title;
+    var newTaskCategorie = '';
+    var newTaskActor = '';
+
+    if (match != null) {
+      newTaskTitle = match[2];
+      newTaskCategorie = match[1];
+      newTaskActor = match[3];
+    }
+
+    var newTaskToCreate = {
+      id: uuidv4(),
+      title: newTaskTitle,
+      actor: newTaskActor,
+      description: '',
+      categorie: newTaskCategorie,
+      estimate: '',
+      order: -1,
+    };
+    console.log(newTaskToCreate);
+    this.tasks.unshift(newTaskToCreate);
+    this.saveOrder();
     this.newTaskForm.reset();
   }
 
+
+  /* ORDER Drag & Drop Action */
   drop(event: CdkDragDrop<Task[]>) {
     moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
     this.saveOrder();
   }
 
+  /* To be able to keep the order define by the user */
   saveOrder() {
     this.tasks.forEach((x,index) => { x.order=index  });
   }
